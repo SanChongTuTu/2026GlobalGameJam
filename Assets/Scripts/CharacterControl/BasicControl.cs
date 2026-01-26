2fc6d1b4148a4b670f74ba831777fd17656c8c41
USKLxsX <19163163718@163.com>
2026-01-26 20:54:26 +0800

    update

--- Assets/Scripts/CharacterControl/BasicControl.cs
+++ Assets/Scripts/CharacterControl/BasicControl.cs
@@ -1,17 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class BasicControl : MonoBehaviour
 {
-    [Header("Movement Settings")]
+    [Header("Player Settings")]
     public float baseMoveSpeed = 5f;
     public float baseJumpForce = 7f;
+    public float playerDamage = 10f;
+    public float attackCooldown = 0.5f;
 
     [Header("Ground Detection")]
     public Transform groundCheck;    // Assign an empty child object here
     public float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;    // Select your "Ground" layer here
+    public LayerMask groundLayer;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
 
     private Rigidbody _rb;
     private float _currentMoveSpeed;
@@ -24,31 +31,35 @@ public class BasicControl : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
         if (transform.childCount > 0)
             _sr = transform.GetChild(0).GetComponent<MeshRenderer>();
-            
+
         _currentMoveSpeed = baseMoveSpeed;
     }
+
+    private float _nextAttackTime = 0f;
 
     void Update()
     {
         if (_isDead) return;
-        
+
         CheckGround();
-        
+
         HandleMovement();
-        
+
         HandleJump();
-        
+
+        Attack();
+
         float h = Input.GetAxis("Horizontal");
+
         //if (h != 0 && _sr != null)
         //{
-        //    _sr.flipX = h > 0; // Note: Depending on sprite, this might need to be h < 0
+        //    _sr.flipX = h > 0;
         //}
     }
 
     void CheckGround()
     {
-        // Creates a small invisible circle at the feet to see if it touches the Ground Layer
-        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
     void HandleJump()
@@ -56,7 +67,7 @@ public class BasicControl : MonoBehaviour
         // Jump only if Space is pressed AND we are touching the ground
         if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
-            _rb.velocity = new Vector2(_rb.velocity.x, baseJumpForce);
+            _rb.velocity = new Vector3(_rb.velocity.x, baseJumpForce, _rb.velocity.z);
         }
     }
 
@@ -82,6 +93,36 @@ public class BasicControl : MonoBehaviour
         {
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+        if (attackPoint != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
+    }
+
+    private void Attack()
+    {
+        if (Time.time >= _nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                Debug.Log("Try to perform attack");
+
+                Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayer);
+
+                foreach (Collider enemy in hitEnemies)
+                {
+                    Monster monster = enemy.GetComponent<Monster>();
+
+                    if (monster != null)
+                    {
+                        monster.TakeDamage(playerDamage);
+                    }
+                }
+
+                _nextAttackTime = Time.time + attackCooldown;
+            }
         }
     }
 }
